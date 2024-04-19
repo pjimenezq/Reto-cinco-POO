d9f27e230c561fd85b938d0ee02be8d5712bff54
pjimenezq <141860508+pjimenezq@users.noreply.github.com>
2024-04-18 20:20:48 -0500

    Update equilateral.py

--- parte_dos/paquete/equilateral.py
+++ parte_dos/paquete/equilateral.py
@@ -1,1 +1,19 @@
+import paquete.triangle as triangle
+
+
+class Equilateral(triangle.Triangle):#Subclase
+    def __init__(self, is_regular: bool, vertices: list = [triangle.shape.point.Point], edges: list = [triangle.shape.line.Line]):
+        super().__init__(is_regular, vertices, edges)#Llamando atributos y métodos de la clase base
+
+    def compute_perimeter(self):#Polimorfismo
+        if self.get_is_regular()==True:
+            return self.get_edges()[0].get_length()*3#El perimetro de un triángulo equilatero es cualquier lado multiplicado por 3
+        else:
+            return "The triangle should be regular."
+        
+    def compute_inner_angles(self):#Poliformismmo
+        if self.get_is_regular()==True:#Todos los ángulos de un triángulo equilatero miden 60 grados
+            return [60, 60, 60]
+        else:
+            return "The triangle should be regular."
 
