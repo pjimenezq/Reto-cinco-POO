04ba6e113a673a88ed13cd2dab63d1d389ae332c
pjimenezq <141860508+pjimenezq@users.noreply.github.com>
2024-04-18 20:21:47 -0500

    Update line.py

--- parte_dos/paquete/line.py
+++ parte_dos/paquete/line.py
@@ -1,1 +1,31 @@
+import paquete.point as point
 
+class Line:
+    def __init__(self, start_point:point.Point, end_point:point.Point): #Hay composición: una línea tiene dos puntos
+        self.__start_point = start_point
+        self.__end_point = end_point
+        self.__length = start_point.compute_distance(end_point) #Length se cálcula con el método compute_distance de la clase Point
+
+    # Setter start point
+    def set_start_point(self, new_start_point):
+        self.__start_point = new_start_point
+
+    # Getter start point
+    def get_start_point(self):
+        return self.__start_point
+    
+    # Setter end point
+    def set_end_point(self, new_end_point):
+        self.__end_point = new_end_point
+
+    # Getter end point
+    def get_end_point(self):
+        return self.__end_point
+    
+    # Setter length
+    def set_length(self, new_length):
+        self.__length = new_length
+
+    # Getter length
+    def get_length(self):
+        return self.__length
