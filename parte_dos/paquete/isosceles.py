3eb1e8fd7fde9e45484b1e27e8c35abe93728d43
pjimenezq <141860508+pjimenezq@users.noreply.github.com>
2024-04-18 20:21:20 -0500

    Update isosceles.py

--- parte_dos/paquete/isosceles.py
+++ parte_dos/paquete/isosceles.py
@@ -1,1 +1,6 @@
+import paquete.triangle as triangle
+
+class Isosceles(triangle.Triangle):#Subclase
+    def __init__(self, is_regular: bool, vertices: list = [triangle.shape.point.Point], edges: list = [triangle.shape.line.Line]):
+        super().__init__(is_regular, vertices, edges)#Llamando atributos y métodos de la clase base
 
